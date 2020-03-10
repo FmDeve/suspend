f0e1212524361e2e7ee42382f29f460d59cd9c2a
dolphin52678 <43961730+dolphin52678@users.noreply.github.com>
2020-03-10 19:12:29 -0400

    Update subway-discord-bot.py

--- subway-discord-bot.py
+++ subway-discord-bot.py
@@ -1,90 +1,111 @@
-import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
-import asyncio
-import time
+const roblox = require('noblox.js');
+const chalk = require('chalk');
+let SuspendRankId = 232
 
-Client = discord.Client() 
-client = commands.Bot(command_prefix = "!") 
-#Nub Army
+async function getRankName(func_group, func_user){
+    let rolename = await roblox.getRankNameInGroup(func_group, func_user);
+    return rolename;
+}
 
-chat_filter = ["APPLE"]
-bypass_list = []
+async function getRankID(func_group, func_user){
+    let role = await roblox.getRankInGroup(func_group, func_user);
+    return role;
+}
 
+async function getRankFromName(func_rankname, func_group){
+    let roles = await roblox.getRoles(func_group);
+    let role = await roles.find(rank => rank.name == func_rankname);
+    if(!role){
+        return 'NOT_FOUND';
+    }
+    return role.rank;
+}
 
-@client.event 
-async def on_ready():
-    print("Bot is online and connected to Discord")
-    await client.send_message(discord.Object(id='558128313009176579'),  "***Bot Restarted!***")
-
-@client.event
-async def on_message(message):
-    if message.content == "cookie":
-        await client.send_message(message.channel, ":cookie:")
-    if message.content == "ping":
-        embed = discord.Embed(colour=0x0dc93f)
-        embed.add_field(name=":timer: Pong!", value="Made by Fm_Developer#4182")
-        await client.send_message(message.channel, embed=embed)
-    if message.content.upper().startswith('!EMBED'):
-        if "558141576770093056" in [role.id for role in message.author.roles]:
-            args = message.content.split(" ")
-            embed = discord.Embed(colour=0xE9A72F)
-            embed.add_field(name="%s" % (" ".join(args[1:])), value="*Bot made by Fm_Developer*")
-            await client.send_message(discord.Object(id='559050683979005983559050683979005983'),  embed=embed)
-        else:
-            await client.send_message(message.channel, "You do not have permission")
-    if message.content.upper().startswith('!AMIADMIN'):
-        if "558141576770093056" in [role.id for role in message.author.roles]: 
-            await client.send_message(message.channel, "You are an admin")
-        else:
-            await client.send_message(message.channel, "You are not an admin")
-    if message.content.upper().startswith('!SAY'):
-        if "558141576770093056" in [role.id for role in message.author.roles]:
-            args = message.content.split(" ")
-            await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
-        else:
-            await client.send_message(message.channel, "You do not have permission")
-    contents = message.content.split(" ") 
-    for word in contents:
-        if word.upper() in chat_filter:
-            if not message.author.id in bypass_list:
-                try:
-                    await client.delete_message(message)
-                    await client.send_message(message.channel, "Stop saying bad words. Your not cool (:icecream:) so stop it. :octagonal_sign:")
-                except discord.errors.NotFound:
-                    return
-    if message.content.upper().startswith('!BAN'):
-        if "558141576770093056" in [role.id for role in message.author.roles]:
-            args = message.content.split(" ")
-            member = discord.utils.get(message.server.members, mention=args[1])
-            await client.send_message(member, "You Have Been Banned From Nub Army")
-            await client.ban(member)
-        else:
-            await client.send_message(message.channel, "You are not an Admin")
-    if message.content.upper().startswith('!KICK'):
-        if "558141576770093056" in [role.id for role in message.author.roles]:
-            args = message.content.split(" ")
-            member = discord.utils.get(message.server.members, mention=args[1])
-            await client.send_message(member, "You Have Been Kicked From Nub Army")
-            await asyncio.sleep(2)
-            await client.kick(member)
-        else:
-            await client.send_message(message.channel, "You are not an Admin")
-    if message.content == "!cmds":
-        embed = discord.Embed(colour=0xE9A72F)
-        embed.add_field(name="**Command List** \n `cookie` - *Gives you a cookie* \n `!say` - *Makes the bot say whatever you want* \n `!embed` - *Creats a announcement* \n `!amIadmin` - *tells you your moderation Status* \n `!cmds2` - *Displays the second list of commands*", value="Made by Fm_Developer#4182")
-        await client.send_message(message.channel, embed=embed)
-    if message.content.upper().startswith('!CMDS2'):
-            embed = discord.Embed(colour=0xE9A72F)
-            embed.add_field(name="**Command List** \n `!kick` - *Kicks the specified user from the discord* \n `!ban` - *Bans the specified user from the discord* \n `!warn`- *warns the user you specify* ", value="Made by Fm_Developer#4182")
-            await client.send_message(message.channel, embed=embed)
-    if message.content == "!bannedwordlist":
-        await client.send_message(message.channel, "Why do you wanna look at innapropriate words? Huh? I am on to you.")
-
-
-
-            
-client.run('NTU5MjA4ODg5ODY3MDQyODQ3.D3iDcA.Uak5HcHGmJaroWCc-m2JOr5wW00')
-
-
-#hex codes -- red- 0xca054d, orange - 0xff7f50, yellow - 0xfbfd3e, green - 0x0dc93f, blue - 0x00e4f6, purple - 0x610083 , magenta - 0xef00ef
+exports.run = async (client, message, args) => {
+    if(!message.member.roles.cache.some(role =>["Ranking Permissions"].includes(role.name))){
+        return message.channel.send({embed: {
+            color: 16733013,
+            description: "You need the `Ranking Permissions` role to run this command.",
+            author: {
+                name: message.author.tag,
+                icon_url: message.author.displayAvatarURL()
+            }
+        }})
+    }
+    let username = args[0];
+    if(!username){
+        return message.channel.send({embed: {
+            color: 16733013,
+            description: "The username argument is required.",
+            author: {
+                name: message.author.tag,
+                icon_url: message.author.displayAvatarURL()
+            }
+        }});
+    }
+    let id;
+    try {
+        id = await roblox.getIdFromUsername(username);
+    } catch {
+        return message.channel.send({embed: {
+            color: 16733013,
+            description: `Oops! ${username} is not a Roblox user. Perhaps you misspelled?`,
+            author: {
+                name: message.author.tag,
+                icon_url: message.author.displayAvatarURL()
+            }
+        }});
+    }
+    let rankInGroup = await getRankID(client.config.groupId, id);
+    let rankNameInGroup = await getRankName(client.config.groupId, id);
+    if(client.config.maximumRank <= rankInGroup){
+        return message.channel.send({embed: {
+            color: 16733013,
+            description: "This rank cannot be ranked by this bot.",
+            author: {
+                name: message.author.tag,
+                icon_url: message.author.displayAvatarURL()
+            }
+        }});
+    }
+    let SuspendResponse;
+    try {
+        SuspendResponse = await roblox.setRank(client.config.groupId, id, SuspendRankId);
+    } catch (err) {
+        console.log(chalk.red('An error occured when running the suspend command: ' + err));
+        return message.channel.send({embed: {
+            color: 16733013,
+            description: `Oops! An unexpected error has occured. It has been logged to the bot console.`,
+            author: {
+                name: message.author.tag,
+                icon_url: message.author.displayAvatarURL()
+            }
+        }});
+    }
+    let newRankName = await getRankName(client.config.groupId, id);
+    message.channel.send({embed: {
+        color: 15542033,
+        description: `**Success!** Suspended ${username} to ${newRankName} (${SuspendResponse.rank})`,
+        author: {
+            name: message.author.tag,
+            icon_url: message.author.displayAvatarURL()
+        }
+    }});
+    if(client.config.logchannelid === 'false') return;
+    let logchannel = await message.guild.channels.cache.get(client.config.logchannelid);
+    logchannel.send({embed: {
+        color: 15542033,
+        description: `<@${message.author.id}> has suspened ${username} from ${rankNameInGroup} (${rankInGroup}) to ${newRankName} (${SuspendResponse.rank}).`,
+        author: {
+            name: message.author.tag,
+            icon_url: message.author.displayAvatarURL()
+        },
+        footer: {
+            text: 'Action Logs'
+        },
+        timestamp: new Date(),
+        thumbnail: {
+            url: `http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username=${username}`
+        }
+    }});
+}
